e53522972e48607446e6d1581bb4483f1b102304
YGF <ygf@cnhonker.com>
2026-03-31 19:15:54 +0800

    Initial Commit

---
+++ HideTitleBarAppDemo/HideTitleBarAppDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HideTitleBarAppDemoApp.swift
+//  HideTitleBarAppDemo
+//
+//  Created by dev on 2026/3/31.
+//
+
+import SwiftUI
+
+@main
+struct HideTitleBarAppDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
